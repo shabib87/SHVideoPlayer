82628d598c8350720d1617b75a6a6fe085734f44
shabib87 <shabib.sust@gmail.com>
2016-11-19 22:10:49 +0600

    SHVideoPlayerView init method created.

--- SHVideoPlayer/Classes/SHVideoPlayer.swift
+++ SHVideoPlayer/Classes/SHVideoPlayer.swift
@@ -12,4 +12,19 @@ import SnapKit
 open class SHVideoPlayer: UIView {
     
     open var tapGesture: UITapGestureRecognizer!
+    fileprivate var playerControllerView: SHVideoPlayerControlable!
+    fileprivate var playerLayer: SHVideoPlayerLayerView!
+    
+    convenience init(withControllerView playerControllerView: SHVideoPlayerControlable) {
+        super.init()
+        self.playerControllerView = playerControllerView
+    }
+    
+    init() {
+        self.playerControllerView = SHVideoPlayerWithDefaultControlsView()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
