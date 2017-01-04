206389cdb3eb27e186de78ebdff91ae4884679dc
shabib87 <shabib.sust@gmail.com>
2017-01-04 09:19:35 +0600

    video player class init method started

--- SHVideoPlayer/Classes/SHVideoPlayer.swift
+++ SHVideoPlayer/Classes/SHVideoPlayer.swift
@@ -15,16 +15,24 @@ open class SHVideoPlayer: UIView {
     fileprivate var playerControllerView: SHVideoPlayerControlable!
     fileprivate var playerLayer: SHVideoPlayerLayerView!
     
-    convenience init(withControllerView playerControllerView: SHVideoPlayerControlable) {
-        super.init()
-        self.playerControllerView = playerControllerView
-    }
-    
-    init() {
-        self.playerControllerView = SHVideoPlayerWithDefaultControlsView()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
     
     required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
+    }
+    
+    public convenience init (customControllView: SHVideoPlayerControlable?) {
+        self.init(frame:CGRect.zero)
+        self.playerControllerView = customControllView
+    }
+    
+    public convenience init() {
+        self.init(customControllView:nil)
+    }
+    
+    deinit {
+        
     }
 }
