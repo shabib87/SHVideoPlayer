acf90843d204691f831a4b834a8de04d2835c01f
shabib87 <shabib.sust@gmail.com>
2017-01-22 16:59:23 +0600

    Preparing player view

--- SHVideoPlayer/Classes/SHVideoPlayer.swift
+++ SHVideoPlayer/Classes/SHVideoPlayer.swift
@@ -8,17 +8,27 @@
 
 import Foundation
 import SnapKit
+import AVFoundation
 
 open class SHVideoPlayer: UIView {
     
     open var tapGesture: UITapGestureRecognizer!
+    open var videoGravity = AVLayerVideoGravityResizeAspect {
+        didSet {
+            self.playerLayer?.videoGravity = videoGravity
+        }
+    }
+    
     fileprivate var playerControl: SHVideoPlayerControl!
     fileprivate var playerLayer: SHVideoPlayerLayer!
     fileprivate var playerScrubber: SHVideoPlayerScrubber!
     
+    fileprivate var customPlayerControl: SHVideoPlayerControl?
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.playerControl = SHVideoPlayerDefaultControl()
+        self.preparePlayer()
         self.playerScrubber = SHVideoPlayerScrubber(with: playerLayer.player!, slider: playerControl.timeSlider!, currentTimeLabel: playerControl.currentTimeLabel!, durationLabel: playerControl.durationLabel!, remainingTimeLabel: playerControl.remainingTimeLabel!, playPauseButton: playerControl.playButton!)
     }
     
@@ -34,6 +44,35 @@ open class SHVideoPlayer: UIView {
     
     public convenience init() {
         self.init(customControllView:nil)
+    }
+    
+    fileprivate func preparePlayerControl() {
+        self.backgroundColor = UIColor.black
+        if let customView = customControllView {
+            controlView = customView
+        } else {
+            controlView =  BMPlayerControlView()
+        }
+        
+        addSubview(controlView.getView)
+        controlView.updateUI(isFullScreen)
+        controlView.delegate = self
+        controlView.getView.snp.makeConstraints { (make) in
+            make.edges.equalTo(self)
+        }
+        
+        tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.tapGestureTapped(_:)))
+        self.addGestureRecognizer(tapGesture)
+    }
+    
+    fileprivate func preparePlayer() {
+        playerLayer = SHVideoPlayerLayer()
+        playerLayer!.videoGravity = videoGravity
+        insertSubview(playerLayer!, at: 0)
+        playerLayer!.snp.makeConstraints { (make) in
+            make.edges.equalTo(self)
+        }
+        self.layoutIfNeeded()
     }
     
     deinit {
