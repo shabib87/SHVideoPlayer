7039bd9eb626f18deccc7b7ad805cfecd10b4163
shabib87 <shabib.sust@gmail.com>
2017-01-22 17:52:53 +0600

    Model for video player created

---
+++ Example/SHVideoPlayer/SHVideo.swift
@@ -0,0 +1,13 @@
+//
+//  SHVideo.swift
+//  SHVideoPlayer
+//
+//  Created by shabib hossain on 1/22/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+struct SHVideo {
+    
+}
